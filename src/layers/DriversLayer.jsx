e5f2bcc6a66fa07a322d0e4be67290ae62972f45
Kirill Shevtsov <kirwork.em@gmail.com>
2020-05-21 21:11:13 +0300

    Adding reverse geocoding

--- src/layers/DriversLayer.jsx
+++ src/layers/DriversLayer.jsx
@@ -1,20 +1,57 @@
 /* eslint-disable global-require */
-import React from 'react';
-import { Marker, Tooltip } from 'react-leaflet';
+import React, { useState, useEffect } from 'react';
+import { Marker } from 'react-leaflet';
 
 import PopupCommon from '../components/popup/PopupCommon';
 import TooltipOnMap from '../components/tooltip/TooltipOnMap';
 
 import drivers from '../database/drivers';
-import commonMarker from '../assets/markers/commonMarker';
-import { truckMarkerDrive, truckMarkerPause, truckMarkerBreakdown } from '../assets/markers/truckMarkers';
+import {
+  truckMarkerDrive,
+  truckMarkerPause,
+  truckMarkerBreakdown,
+} from '../assets/markers/truckMarkers';
+
+const ENDPOINT_API_URL = process.env.REACT_APP_LOCATIONIQ_ENDPOINT_URL;
+const API_KEY = process.env.REACT_APP_LOCATIONIQ_API_KEY;
 
 function DriversLayer() {
+  const [activeDriverCoordinates, setActiveDriver] = useState([]);
+  const [driverCurrentLocation, setDriverCurrentLocation] = useState('');
+
+  const API_URL = `${ENDPOINT_API_URL}/v1/reverse.php?key=${API_KEY}&lat=${activeDriverCoordinates[0]}&lon=${activeDriverCoordinates[1]}&format=json`;
+  useEffect(() => {
+    if (activeDriverCoordinates.length) {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(API_URL);
+          const fetchedData = await response.json();
+          setDriverCurrentLocation(fetchedData);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      fetchData();
+    }
+  }, [API_URL, activeDriverCoordinates.length]);
+
+  console.log(driverCurrentLocation);
+
   const driversPosition = drivers.map((driver) => (
     <Marker
       key={driver.id}
       position={driver.currentPosition}
-      icon={driver.driverStatus < 0 ? truckMarkerBreakdown : (driver.driverStatus === 0 ? truckMarkerDrive : truckMarkerPause)}
+      icon={
+        driver.driverStatus < 0
+          ? truckMarkerBreakdown
+          : driver.driverStatus === 0
+            ? truckMarkerDrive
+            : truckMarkerPause
+      }
+      onClick={() => {
+        setActiveDriver(driver.currentPosition);
+      }}
     >
       <PopupCommon
         headerContent={`${driver.firstName} ${driver.lastName}`}
@@ -22,13 +59,12 @@ function DriversLayer() {
         isBroken={driver.driverStatus < 0}
         isDriving={driver.driverStatus === 0}
         isStopped={driver.driverStatus > 0}
+        currentLocation={driverCurrentLocation.display_name}
       />
       <TooltipOnMap tooltipContent={driver.firstName} />
     </Marker>
   ));
-  return (
-    <div>{driversPosition}</div>
-  );
+  return <div>{driversPosition}</div>;
 }
 
 export default DriversLayer;
